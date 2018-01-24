89bb045308ff5f01df984a1551d9e13ad28021f0
NicolaZoccarato <nicolazoccarato@hotmail.it>
2018-01-24 22:57:51 +0100

    c

--- js/web.js
+++ js/web.js
@@ -18,19 +18,37 @@ $(document).ready(function() {
 });
 
 
-	$('#btn-block').click(function(){
-		$.get( 'https://api.blockcypher.com/v1/btc/main/blocks/' +$('#input-block').val(), function(data) {
-				var sortie = '';
+	$('#single-block').click(function(){
+		$.get( 'https://api.blockcypher.com/v1/btc/main/blocks/' +$('#input').val(), function(data) {
+				var a = '';
 			if(data.error != undefined){
-				sortie += data.error;
+				a += data.error;
 			}
 			else{
-				sortie += '</p><h3> Détails du block</h3> <table class="table">' +
-				'<tr><th>Hash</th><td> '+data.hash+'</td></tr> <tr><th>Hauteur</th><td>'+data.height
-				+'</td></tr> <tr><th>Date</th><td>'+data.time +'</td></tr> <tr><th>Taille (bytes)</th><td>'
+				a += '</p><h3> Details of the block</h3> <table class="table">' +
+				'<tr><th>Hash</th><td> '+data.hash+'</td></tr> <tr><th>Height</th><td>'+data.height
+				+'</td></tr> <tr><th>Date</th><td>'+data.time +'</td></tr> <tr><th>Size (bytes)</th><td>'
 				 +data.size + '</td></tr> </table> ';
+			}
+			$('#result').html( a );
+			});
+	});
+
+
+	$('#transaction').click(function(){
+		$.get( 'https://api.blockcypher.com/v1/btc/main/txs/' +$('#input1').val(), function(data) {
+				var b = '';
+			if(data.error != undefined){
+				b += data.error;
+			}
+			else{
+				b +='</p><h3> Details of the transaction</h3> <table class="table">' +
+				'<tr><th>Hash</th><td>'+data.hash+'</td></tr> <tr><th>Date</th><td>'+data.time +
+				'</td></tr> <tr><th>Size (bytes)</th><td>' +data.size  
+				+'</td></tr> <tr><th>Height (-1 if not validated)</th><td>' + data.block_height 
+				+ '</td></tr> <tr><th>Hash of the black (bytes)</th><td>' + data.block_hash + '</td></tr> </table> ';
 
 			}
-			$('#result').html( sortie );
+			$('#result').html( b );
 			});
 	});
