9427bcebea0ad93cfb1c8d608d6e49014f69121f
NicolaZoccarato <nicolazoccarato@hotmail.it>
2018-01-24 21:25:47 +0100

    c

--- js/web.js
+++ js/web.js
@@ -16,3 +16,21 @@ $(document).ready(function() {
 		}
 	});
 });
+
+
+	$('#btn-block').click(function(){
+		$.get( 'https://api.blockcypher.com/v1/btc/main/blocks/' +$('#input-block').val(), function(data) {
+				var sortie = '';
+			if(data.error != undefined){
+				sortie += data.error;
+			}
+			else{
+				sortie += '</p><h3> Détails du block</h3> <table class="table">' +
+				'<tr><th>Hash</th><td> '+data.hash+'</td></tr> <tr><th>Hauteur</th><td>'+data.height
+				+'</td></tr> <tr><th>Date</th><td>'+data.time +'</td></tr> <tr><th>Taille (bytes)</th><td>'
+				 +data.size + '</td></tr> </table> ';
+
+			}
+			$('#resultat').html( sortie );
+			});
+	});
